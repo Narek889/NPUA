fbc00cfeb3128287f0172cea995608211812dcd9
Narek889 <narek_stepanyan@hotmail.com>
2024-12-10 23:28:07 +0400

    V.10122024

---
+++ Ալգորիթմների տեսություն/Linear Search.py
@@ -0,0 +1,15 @@
+def linear_search(arr, target):
+    for index, value in enumerate(arr):
+        if value == target:
+            return index
+    return -1
+
+# Օրինակ
+arr = [100, 500, 1000, 15000, 6]
+target = 1000
+
+result = linear_search(arr, target)
+if result != -1:
+    print(f"Գտնվեց {result}֊րդ ինդեքսում")
+else:
+    print("Չգտնվեց")
