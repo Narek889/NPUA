3c5763ea975c2cc1ca75cfa745a06f8a21862bee
Narek889 <narek_stepanyan@hotmail.com>
2025-04-18 14:11:58 +0400

    add Find if Path Exists in Graph solution

---
+++ Ալգորիթմների տեսություն/Semester II/Find if Path Exists in Graph.py
@@ -0,0 +1,33 @@
+def find(edges, n, source, destination, visited=None):
+    if visited is None:
+        visited = set()
+
+    # Convert edges to an adjacency list
+    graph = {i: [] for i in range(n)}
+    for edge in edges:
+        graph[edge[0]].append(edge[1])
+        graph[edge[1]].append(edge[0])  # Assuming the graph is undirected
+
+    if source not in visited:
+        print(source)
+        visited.add(source)
+        for neighbor in graph[source]:
+            if neighbor == destination:
+                return True
+            if find(edges, n, neighbor, destination, visited):
+                return True
+    return False
+
+# Example 1
+edges = [[0, 1], [1, 2], [2, 0]]
+n = 3
+destination = 2
+source = 0
+print(f"ex 1 = {find(edges, n, source, destination)}")  # Output: ex 1 = True
+
+# Example 2
+edges_2 = [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]]
+n_2 = 6
+destination_2 = 5
+source_2 = 0
+print(f"ex 2 = {find(edges_2, n_2, source_2, destination_2)}")  # Output: ex 2 = False
